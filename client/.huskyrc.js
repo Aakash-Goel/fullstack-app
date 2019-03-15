e6f8a7ef895b46a8f1d0b0945e9bcfd9146e24af
Aakash Goel <agoel36@sapient.com>
2019-03-16 02:05:25 +0530

    feat(husky): add pre commit hook

---
+++ client/.huskyrc.js
@@ -0,0 +1,49 @@
+const commitmsg = `
+# Hey there
+#
+# We just wanted to let you know that we care a great deal about
+# making our git history clean, maintainable and easy to access for
+# all our contributors. Commit messages are very important to us,
+# which is why we have a strict commit message policy in place.
+# Please use the following guidelines to format all your commit
+# messages:
+#
+#     <type>: <subject>
+#     <type>(<scope>): <subject>
+#
+# Please note that:
+#  - The HEADER is a single line of max. 50 characters that
+#    contains a succinct description of the change. It contains a
+#    type, an optional scope, and a subject
+#       + <type> describes the kind of change that this commit is
+#                providing. Allowed types are:
+#             * feat (feature)
+#             * fix (bug fix)
+#             * docs (documentation)
+#             * style (formatting, missing semicolons, …)
+#             * refactor
+#             * test (when adding missing tests)
+#             * chore (maintain)
+#             * perf (performance)
+#       + <scope> can be anything specifying the place of the commit
+#                 change
+#       + <subject> is a very short description of the change, in
+#                   the following format:
+#             * imperative, present tense: “change” not
+#               “changed”/“changes”
+#             * no capitalised first letter
+#             * no dot (.) at the end
+#
+# Examples:
+#   - fix: feature name bug fix
+#   - feat(feature-name) feature added
+#
+# Thank you
+`;
+
+module.exports = {
+  hooks: {
+    'pre-commit': `lint-staged && echo \"${commitmsg}\"`,
+    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
+  },
+};
